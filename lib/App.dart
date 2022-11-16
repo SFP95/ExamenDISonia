51de6674dd6d5358b03ba106d2122b33ab791305
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-16 12:20:35 +0100

    commit creacion main y app

---
+++ lib/App.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget{
+
+  App ({Key?key}):super (key: key);
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    MaterialApp materialAppMobile=const MaterialApp();
+
+    if(DataHolder().platformAdmin.isAndroidPlatform() ||
+        DataHolder().platformAdmin.isIOSPlatform()){
+      //print("ENTRO EN ANDROID O IOS");
+      materialAppMobile=MaterialApp(
+        theme: ThemeData(
+            backgroundColor: Colors.purple[100],
+            fontFamily:'Reach Story'
+        ),
+        title: 'MyfirstFlutter',
+        initialRoute: '/splashView',
+        routes: {
+          '/home': (context) => HomeView(),
+          '/loginPhoneView': (context) => LoginPhoneView(),
+          '/loginView': (context) => LoginView(),
+          '/registerView': (context) => RegisterView(),
+          '/onBoarding': (context) => OnBoardingView(),
+          '/splashView': (context) => SVLogoWait("assets/images/Logo.png"),
+          //'/prueba': (context) => PruebaNewView(),
+          '/chatView': (context) => ChatView(),
+        },
+      );
+    }
+    else{
+      materialAppMobile=MaterialApp(
+        initialRoute: '/splashView',
+        routes: {
+          '/home': (context) => HomeView(),
+          '/loginPhoneView': (context) => LoginPhoneView(),
+          '/loginView': (context) => LoginView(),
+          '/registerView': (context) => RegisterView(),
+          '/onBoarding': (context) => OnBoardingView(),
+          '/splashView': (context) => SVLogoWait("assets/images/Logo.png"),
+          //'/prueba': (context) => PruebaNewView(),
+          '/chatView': (context) => ChatView(),
+        },
+      );
+    }
+    //print("La PLATAFORMA ES ..... --> "+DataHolder().platformAdmin.isAndroidPlatform().toString());
+
+    return materialAppMobile;
+  }
+}
+  
