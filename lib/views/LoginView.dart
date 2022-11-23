36137ab1e2a3032d4188814eb5f0f721f5a27b5b
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-23 08:55:25 +0100

    inclusion rutas login y contruccion de clase login

--- lib/views/LoginView.dart
+++ lib/views/LoginView.dart
@@ -1,10 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget{
+
+  LoginView ({Key?key}):super (key: key);
+
+  late RFInputText inputUser= RFInputText(
+    titulo: 'USUARIO:',
+    ayuda: 'MrPotato@gmai.com',);
+  late RFInputText inputPsswd= RFInputText(
+    titulo: 'PASSWORD:',
+    ayuda: 'No menos de 8 caracteres',
+    blIsPsswd: true,);
+
+  void LogPressed(RFInputText inputUser, RFInputText inputPsswd, BuildContext context) async{
+    try{
+      final credential= await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: inputUser.getText(),
+        password: inputPsswd.getText(),
+      );
+      print("ESTOY LOGEADO");
+      Navigator.of(context).popAndPushNamed('/onBoarding');
+    }on FirebaseAuthException catch (e){
+      if(e.code=='user-not-found'){
+        print('No user found for that email');
+      }else if(e.code=='wrong-password'){
+        print('Wrong password provided for that user');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white24,
+        title: Text('Login',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 10,
+        ),),
+      ),
+    );
   }
   
 }
