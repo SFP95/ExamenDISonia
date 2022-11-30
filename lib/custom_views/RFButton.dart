4f09669baa7004e2d08d57d0055c7f16e33a8eca
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-30 08:58:46 +0100

    construccion RFButton

--- lib/custom_views/RFButton.dart
+++ lib/custom_views/RFButton.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class RFButton extends StatelessWidget{
+  final String img;
+  final String texto;
+  final TextEditingController myController = TextEditingController();
+
+
+  RFButton({Key? key, this.img="" , this.texto=""}): super (key:key);
+
+  String getText(){
+    return myController.text;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return ElevatedButton(
+        onPressed: (){
+          Navigator.of(context).popAndPushNamed('/home');
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/dragon.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        )
+    );
+
   }
   
 }
